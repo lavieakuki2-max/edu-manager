afedf30a4c1b628cb05b876a1c8df02385a8c322
lavie <lavieakuki2@gmail.com>
2026-08-02 11:34:37 +0200

    nouvelle modification

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,13 +17,8 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
 
-        $middleware->trustProxies(
-            at: '*',
-            headers: \Illuminate\Http\Request::HEADER_X_FORWARDED_FOR
-                | \Illuminate\Http\Request::HEADER_X_FORWARDED_HOST
-                | \Illuminate\Http\Request::HEADER_X_FORWARDED_PORT
-                | \Illuminate\Http\Request::HEADER_X_FORWARDED_PROTO
-        );
+        // Fait confiance à tous les proxys (nécessaire pour Render)
+        $middleware->trustProxies(at: '*');
 
         $middleware->alias([
             'role' => \App\Http\Middleware\RoleMiddleware::class,
